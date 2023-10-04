006d6652d46f52f79899788366fe81a5c6feff25
Matheus Buenevides <matheusgomesbuenevides@gmail.com>
2023-10-04 17:29:37 -0300

    Novos estilos e retorno de dados do cep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DetailsComponent } from './components/details/details.component';
 import { SearchComponent } from './components/search/search.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,10 @@ import { SearchComponent } from './components/search/search.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SearchService } from 'src/app/services/search/search.service';
+import { FormBuilder, } from '@angular/forms';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
+
+  public address = {
+    addressLine: '',
+    city: '',
+    state:''
+  }
+
+  cepForm = this.formBuilder.group({
+    valueCEP: ''
+  });
+
+  constructor (
+    private search: SearchService,
+    private formBuilder: FormBuilder,
+    private changeDetectorRef: ChangeDetectorRef
+   ){}
+
+
+  getDataFromCEP() {
+    this.search.getDetailsCEP(this.cepForm.value.valueCEP).subscribe((data) => this.setData(data)) 
+  }
+
+  setData(data: any) {
+    this.address.addressLine = `${data.logradouro} - ${data.bairro}`;
+    this.address.city = data.localidade;
+    this.address.state = data.uf;
+
+    this.changeDetectorRef.detectChanges();
+  }
 
 }
